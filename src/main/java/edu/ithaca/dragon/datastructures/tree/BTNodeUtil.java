5c7a0b428393523a1585d19e5d8afbb5302b20a2
Toby Dragon <dr.toby.dragon@gmail.com>
2020-11-12 09:27:18 -0500

    first commit

---
+++ src/main/java/edu/ithaca/dragon/datastructures/tree/BTNodeUtil.java
@@ -0,0 +1,29 @@
+package edu.ithaca.dragon.datastructures.tree;
+
+public class BTNodeUtil {
+
+    public static int nodeCount(BTNode tree){
+        return -1;
+    }
+
+    public static int totalOfKeys(BTNode tree){
+        return -1;
+    }
+
+    public static boolean contains(BTNode tree, int keyToFind){
+        return false;
+    }
+
+    public static String preOrderString(BTNode tree){
+        return null;
+    }
+
+    public static String inOrderString(BTNode tree){
+        return null;
+    }
+
+    public static String postOrderString(BTNode tree){
+        return null;
+    }
+    
+}
